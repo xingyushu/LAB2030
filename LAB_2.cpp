f6d07f169e2dfaab5818c038f46565db5ccd2b5c
xingyushu <1402598162@qq.com>
2022-09-27 15:46:34 -0400

    Create LAB_2.cpp

---
+++ LAB_2.cpp
@@ -0,0 +1,31 @@
+// Write a program that asks the user to enter a weight given in pounds and ounces, where you store
+// pounds in a variable of type int and ounces in a variable of type double. Convert this weight to
+// kilograms and grams, where kilograms is stored in a variable of type int and grams is stored in a
+// variable of type double.
+  
+  
+  
+
+//The code 
+
+//Function 1
+// void getInput() :The first function should be used to get two input values 
+
+
+//Function 2
+//void transferParameters( pounds, ounces, kilograms, and grams): After it is run,
+// this function will have changed the values of kilograms and grams so that they represent an
+// equivalent weight to our original input value in pounds and ounces. Here, you will still need to use
+// the pass-by-reference mechanism
+
+//Function 3
+//output the result of the calculation
+
+//Fnuction main()
+//Good comment is needed 
+
+//Members contribution
+
+
+
+
